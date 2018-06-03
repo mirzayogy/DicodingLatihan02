1a0f4b087633c0dbee6b8e948c114f3069486d32
mirzayogy <mirza.yogy@gmail.com>
2018-06-03 15:36:10 +0800

    Intent pindah activity with data

--- app/src/main/java/com/mirzayogy/dicodinglatihan02/MainActivity.java
+++ app/src/main/java/com/mirzayogy/dicodinglatihan02/MainActivity.java
@@ -8,12 +8,16 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button btnMoveActivity;
+    private Button btnMoveWithDataActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnMoveActivity = (Button)findViewById(R.id.btn_move_activity);
         btnMoveActivity.setOnClickListener(this);
+        btnMoveWithDataActivity = (Button)findViewById(R.id.btn_move_activity_data);
+        btnMoveWithDataActivity.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
@@ -21,6 +25,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_move_activity:
                 Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
                 startActivity(moveIntent);
+                break;
+
+            case R.id.btn_move_activity_data:
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy Boy");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5);
+                startActivity(moveWithDataIntent);
                 break;
         }
     }
