da8dc6b8a780fb2f91024599f7bfa5f68b587c1a
mirzayogy <mirza.yogy@gmail.com>
2018-06-04 06:53:23 +0800

    intent implisit (DIAL NUMBER)

--- app/src/main/java/com/mirzayogy/dicodinglatihan02/MainActivity.java
+++ app/src/main/java/com/mirzayogy/dicodinglatihan02/MainActivity.java
@@ -1,6 +1,7 @@
 package com.mirzayogy.dicodinglatihan02;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnMoveActivity;
     private Button btnMoveWithDataActivity;
     private Button btnMoveWithObject;
+    private Button btnDialPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnMoveWithDataActivity.setOnClickListener(this);
         btnMoveWithObject = (Button)findViewById(R.id.btn_move_activity_object);
         btnMoveWithObject.setOnClickListener(this);
+        btnDialPhone = (Button)findViewById(R.id.btn_dial_number);
+        btnDialPhone.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
@@ -46,6 +50,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent moveWithObjectIntent = new Intent(MainActivity.this, MoveWithObjectActivity.class);
                 moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, mPerson);
                 startActivity(moveWithObjectIntent);
+                break;
+
+            case R.id.btn_dial_number:
+                String phoneNumber = "081210841382";
+                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phoneNumber));
+                startActivity(dialPhoneIntent);
                 break;
         }
     }
